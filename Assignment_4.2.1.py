0dcf2e80a500ddedb318c8a0c28bdd8e5ee2d1e8
silpa016 <43297600+silpa016@users.noreply.github.com>
2018-09-24 09:02:40 +0530

    Add files via upload

---
+++ Assignment_4.2.1.py
@@ -0,0 +1,8 @@
+listOfWords = ['silpa','rani','giiita','swarna1']
+listOfInts = []
+ 
+for i in range(len(listOfWords)):
+    listOfInts.append(len(listOfWords[i]))
+ 
+print ("List of words:"+str(listOfWords))    
+print ("List of wordlength:"+str(listOfInts))
